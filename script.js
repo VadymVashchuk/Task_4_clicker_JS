fc370b5f302b3ace7c129b0d3e614abff15b1dcb
VadymVashchuk <114853541+VadymVashchuk@users.noreply.github.com>
2022-10-21 21:12:59 +0300

    completed_base_with_score

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const enemy = document.getElementById('enemy');
+const score = document.getElementById('score');
+scoreValue = score.innerHTML;
 
 function hit() {
 enemy.style.top = Math.random() * 350+"px";
 enemy.style.left = Math.random() * 650+"px";
+++scoreValue;
+console.log(scoreValue);
+score.innerHTML = scoreValue;
 }
 
 enemy.addEventListener("click", hit);
-
-
-
-
-
